d5a2271185cb9fad558734ef2458f1e625cca578
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-08-11 23:17:21 +0900

    fix: copilot の指摘を修正

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
   devtools: { enabled: true },
   compatibilityDate: "2025-07-15",
 });
