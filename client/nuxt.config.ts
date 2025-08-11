194e3c1e356bee6cddd41c1353b91e0f2e6ee3d1
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-08-11 11:09:34 +0900

    chore: ESLintの導入

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/eslint"],
 });
