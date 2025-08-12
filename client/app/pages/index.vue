d33af9bb6cb0744b7ae4db64783868a37155bc71
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-08-12 12:40:52 +0900

    fix: Carouselをテストする際に使用する画像を変更

--- client/app/pages/index.vue
+++ client/app/pages/index.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 const items = [
-  "https://picsum.photos/640/640?random=1",
-  "https://picsum.photos/640/640?random=2",
-  "https://picsum.photos/640/640?random=3",
-  "https://picsum.photos/640/640?random=4",
-  "https://picsum.photos/640/640?random=5",
-  "https://picsum.photos/640/640?random=6",
+  "https://placehold.jp/3d4070/ffffff/800x600.png?text=Carousel%E2%86%92",
+  "https://placehold.jp/07a0a5/ffffff/800x600.png?text=%E2%86%90Carousel",
 ];
 </script>
 
