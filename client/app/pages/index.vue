8bf09ab3af98194ae81208bc8a340d26e89d7ac5
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-08-17 20:25:28 +0900

    chore: NeoShowcase と競合しないのか確認

--- client/app/pages/index.vue
+++ client/app/pages/index.vue
@@ -3,6 +3,14 @@ const items = [
   'https://placehold.jp/3d4070/ffffff/800x600.png?text=Carousel%E2%86%92',
   'https://placehold.jp/07a0a5/ffffff/800x600.png?text=%E2%86%90Carousel',
 ];
+
+const { data, error, status } = useFetch('/api/ping');
+if (error.value) {
+  console.error('GETリクエストエラー:', error.value);
+}
+else {
+  console.log('GETリクエスト結果:', data.value);
+}
 </script>
 
 <template>
