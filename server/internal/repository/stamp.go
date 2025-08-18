3e9e49309b642f9faf482862c824537f52396038
mamo <p04snao11@gmail.com>
2025-08-18 17:01:45 +0900

    変更途中

---
+++ server/internal/repository/stamp.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+
+
+)
+
+type (
+	// users table
+	Stamp struct {
+		ID    uuid.UUID `db:"id"`
+		Name  string    `db:"name"`
+		FileID string    `db:"file_id"`
+		CreatorID uuid.UUID `db:"creator_id"`
+		IsUnicode Boolean `db:"is_unicode"`
+		createdAt  string `db:"created_at"`
+	}
+
+	CreateUserParams struct {
+		Name  string
+		Email string
+	}
+)
+
+func (r *Repository) getStamps(ctx context.Context) ([]*User, error) {
+	users := []*User{}
+	if err := r.db.SelectContext(ctx, &users, "SELECT * FROM users"); err != nil {
+		return nil, fmt.Errorf("select users: %w", err)
+	}
+
+	return users, nil
+}
+
+func (r *Repository) getCertainStamps(ctx context.Context) ([]*User, error) {
+	users := []*User{}
+	if err := r.db.SelectContext(ctx, &users, "SELECT * FROM users"); err != nil {
+		return nil, fmt.Errorf("select users: %w", err)
+	}
+
+	return users, nil
+}
+
+func (r *Repository) getCertainStampDetails(ctx context.Context) ([]*User, error) {
+	users := []*User{}
+	if err := r.db.SelectContext(ctx, &users, "SELECT * FROM users"); err != nil {
+		return nil, fmt.Errorf("select users: %w", err)
+	}
+
+	return users, nil
+} 
