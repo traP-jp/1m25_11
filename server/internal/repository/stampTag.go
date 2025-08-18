36644696ca4e005016bfffd1d9c21da7de1d1546
Ryuya HASEGAWA <ryhaxa@gmail.com>
2025-08-18 17:02:28 +0900

    CreateStampTagsParams構造体のupdatedAtフィールドの修正と、CreateStampTagsメソッドのSQL文を更新

--- server/internal/repository/stampTag.go
+++ server/internal/repository/stampTag.go
@@ -8,23 +8,26 @@ import (
 
 type (
 	StampTag struct {
-		name      string    `db:"name"`
-		createrID uuid.UUID `db:"creater_id"`
-		createdAt string    `db:"created_at"` // string型は流石にない
-		updateAt  string    `db:"update_at"`
+		id        uuid.UUID      `db:"id"`
+		name      string         `db:"name"`
+		createrID uuid.UUID      `db:"creater_id"`
+		createdAt string         `db:"created_at"`
+		updatedAt string         `db:"updated_at"`
+		count     int            `db:"count"`
+		stamps    []stampSummary `db:"stamps"`
 	}
 
 	CreateStampTagsParams struct {
 		name      string
 		createrID uuid.UUID
 		createdAt string
-		updateAt  string
+		updatedAt string
 	}
 )
 
 func (r *Repository) CreateStampTags(ctx context.Context, params CreateStampTagsParams) (uuid.UUID, error) {
 	stampTagID := uuid.New()
-	if _, err := r.db.ExecContext(ctx, "INSERT INTO stamp_tags (id, name, creater_id) VALUES (?, ?, ?)", stampTagID, params.name, params.createrID, params.createdAt, params.updateAt); err != nil {
+	if _, err := r.db.ExecContext(ctx, "INSERT INTO stamp_tags (id, name, creater_id) VALUES (?, ?, ?)", stampTagID, params.name, params.createrID, params.createdAt, params.updatedAt); err != nil {
 		return uuid.Nil, err
 	}
 
