292ab505090b155204eb4f5d9b3a78e56450daa0
mamo <p04snao11@gmail.com>
2025-08-19 15:39:59 +0900

    Failをfailに変換

--- server/internal/repository/tag.go
+++ server/internal/repository/tag.go
@@ -35,7 +35,7 @@ func (r *Repository) GetTags(ctx context.Context) ([]*Tag, error) {
 
 func (r *Repository) UpdateTags(ctx context.Context, tagID uuid.UUID, name string)error{
 	if _, err :=r.db.ExecContext(ctx,`UPDATE tags SET name = ? WHERE id = ?`, name, tagID); err != nil{
-		return fmt.Errorf("Failed to update tag: %w",err)
+		return fmt.Errorf("failed to update tag: %w",err)
 	}
 
 	return nil
@@ -43,7 +43,7 @@ func (r *Repository) UpdateTags(ctx context.Context, tagID uuid.UUID, name strin
 
 func (r *Repository) DeleteTags(ctx context.Context, tagID uuid.UUID)error{
 	if _,err := r.db.ExecContext(ctx, `DELETE FROM tags WHERE id=?`, tagID); err != nil{
-		return fmt.Errorf("Failed to delete tag: %w", err)
+		return fmt.Errorf("failed to delete tag: %w", err)
 	}
 
 	return nil
@@ -52,7 +52,7 @@ func (r *Repository) DeleteTags(ctx context.Context, tagID uuid.UUID)error{
 func (r *Repository) CreateTags(ctx context.Context, params CreateTagParams)(uuid.UUID, error){
 	tagID := uuid.New()
 	if _, err := r.db.ExecContext(ctx, "INSERT INTO tags(id, name, creator_id, created_at, updated_at) VALUES(?,?,?,?,?)", tagID, params.Name, params.CreatorID, params.CreatedAt, params.UpdatedAt);err != nil{
-		return uuid.Nil, fmt.Errorf("Failed to insert tag: %w", err )
+		return uuid.Nil, fmt.Errorf("failed to insert tag: %w", err )
 	}
 
 	return tagID, nil
