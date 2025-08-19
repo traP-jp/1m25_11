4d0e6442455c8920a5d019532dc7b8acf3e52de1
mamo <p04snao11@gmail.com>
2025-08-19 11:58:25 +0900

    stamptag.goの作成

--- server/internal/repository/stampTag.go
+++ server/internal/repository/stampTag.go
@@ -2,41 +2,42 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
 )
 
 type (
-	StampSummary struct {
-		id      uuid.UUID
-		name    string
-		file_id string
-	}
-	// StampSummaryの定義は他のファイルで行われたほうがいい気がする
-
-	StampTag struct {
-		id        uuid.UUID      `db:"id"`
-		name      string         `db:"name"`
-		createrID uuid.UUID      `db:"creater_id"`
-		createdAt string         `db:"created_at"`
-		updatedAt string         `db:"updated_at"`
-		count     int            `db:"count"`
-		stamps    []StampSummary `db:"stamps"`
-	}
-
-	CreateStampTagsParams struct {
-		name      string
-		createrID uuid.UUID
-		createdAt string
-		updatedAt string
+	CreateStampTagParams struct{
+		StampID uuid.UUID `db:"stamp_id"`
+		TagID uuid.UUID `db:"tag_id"`
+		CreatorID uuid.UUID `db:"creator_id"`
 	}
 )
 
-func (r *Repository) CreateStampTags(ctx context.Context, params CreateStampTagsParams) (uuid.UUID, error) {
-	stampTagID := uuid.New()
-	if _, err := r.db.ExecContext(ctx, "INSERT INTO stamp_tags (id, name, creater_id) VALUES (?, ?, ?)", stampTagID, params.name, params.createrID, params.createdAt, params.updatedAt); err != nil {
-		return uuid.Nil, err
+func (r *Repository) CreateStampTags(ctx context.Context, params CreateStampTagParams) error {
+	if _, err := r.db.ExecContext(ctx, "INSERT INTO stamp_tags (stamp_id, tag_id, creator_id) VALUES (?, ?, ?)", params.StampID, params.TagID, params.CreatorID); err != nil {
+		return  fmt.Errorf("failed to insert stampTags:%w",err)
 	}
 
-	return stampTagID, nil
+	return nil
 }
+
+func (r *Repository) DeleteStampTags(ctx context.Context, stampID uuid.UUID, tagID uuid.UUID)error{
+	if _, err := r.db.ExecContext(ctx, "DELETE FROM stamp_tags WHERE stamp_id = ? AND tag_id = ?",stampID,tagID); err != nil{
+		return fmt.Errorf("failed to delete stampTag:%w",err)
+	}
+
+	return nil
+}
+
+
+func (r *Repository) GetSearchStampTags(ctx context.Context, keyword string)([]uuid.UUID, error){
+	stampIDs := []uuid.UUID{}
+	if err := r.db.SelectContext(ctx, &stampIDs,"SELECT stamp_id FROM stamp_tags JOIN tags ON stamp_tags.tag_id = tags.id WHERE tags.name LIKE ?","%"+keyword+"%"); err !=nil {
+		return nil,fmt.Errorf("failed to get stampID by tagKeyword : %w",err)
+	}
+
+	return stampIDs,nil
+}
+
