ca4f142f966e06768bd03c19004eac43785fa41f
mamo <p04snao11@gmail.com>
2025-08-20 23:37:00 +0900

    いらないものを削除

--- server/internal/handler/stamp.go
+++ server/internal/handler/stamp.go
@@ -2,22 +2,7 @@ package handler
 
 import (
 	"net/http"
-	"time"
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
-)
-type (
-	stamp struct{
-		ID         uuid.UUID `json:"id"`
-		Name        string `json:"name"`
-		FileID      uuid.UUID `json:"file_id"`
-		CreatorID   uuid.UUID  `json:"creator_id"`
-		IsUnicode   bool   `json:"is_unicode"`
-		CreatedAt   time.Time `json:"created_at"`
-		UpdatedAt   time.Time `json:"updated_at"`
-		CountMonthly int    `json:"count_monthly"`
-		CountTotal   int64  `json:"count_total"`	
-	}
 )
 
 func (h *Handler) getStamps(c echo.Context) error {
