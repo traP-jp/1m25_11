078c95989f27e3c0e7e33fa7c43dc98cbf55cc0d
ikea-6330 <ide.k.624d@m.isct.ac.jp>
2025-08-20 16:51:46 +0900

    Carendarの表示

--- client/app/components/SearchInputModal.vue
+++ client/app/components/SearchInputModal.vue
@@ -14,10 +14,25 @@
       placeholder="スタンプ名で検索"
       class="mb-5 w-full"
     />
-  </UContainer>
+    <UFormField
+      label="タグ名を指定"
+      required
+    >
+      <UInputTags
+        :v-model="searchStampTagValue"
+        placeholder="タグ名を入力"
+        class="mb-5 w-full"
+      />
+    </UFormField>
+    <p>作成日を指定</p>
+    <CarendarModal />
+  </ucontainer>
 </template>
 
 <script setup lang="ts">
+import CarendarModal from './CarendarModal.vue';
+
 const searchDescriptionValue = ref('');
 const searchStampNameValue = ref('');
+const searchStampTagValue = ref(['Vue']);
 </script>
