516006b183c5d3131ef4aaf74cb65fb95507e44e
ikea-6330 <ide.k.624d@m.isct.ac.jp>
2025-08-20 13:55:57 +0900

    feat:検索バーの表示(一部)

--- client/app/components/SearchInputModal.vue
+++ client/app/components/SearchInputModal.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>aaa</div>
+  <UContainer>
+    <p>説明文で検索</p>
+    <UInput
+      v-model="searchDescriptionValue"
+      icon="i-lucide-search"
+      placeholder="説明文で検索"
+      class="mb-5 w-full"
+    />
+    <p>スタンプ名で検索</p>
+    <UInput
+      v-model="searchStampNameValue"
+      icon="i-lucide-search"
+      placeholder="スタンプ名で検索"
+      class="mb-5 w-full"
+    />
+  </UContainer>
 </template>
 
 <script setup lang="ts">
+const searchDescriptionValue = ref('');
+const searchStampNameValue = ref('');
 </script>
