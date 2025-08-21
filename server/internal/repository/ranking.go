037cb6761e3b8b4531d71f2ebfbcd186ff76c05e
hachimitsu-pichi <hachimitsu.38@icloud.com>
2025-08-21 10:39:43 +0900

    ranking 修正

--- server/internal/repository/ranking.go
+++ server/internal/repository/ranking.go
@@ -3,29 +3,34 @@ package repository
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type StampCountResult struct {
-	StampID string `db:"stamp_id"`
-	Count   int    `db:"total_count"`
+	StampID       uuid.UUID `db:"stamp_id"`
+	ReactionCount int       `db:"reaction_count"`
+	MessageCount  int       `db"message_count"`
 }
 
-func (r *Repository) GetStampCount(ctx context.Context, Since *openapi_types.Date, Until *openapi_types.Date) ([]StampCountResult, error) {
+func (r *Repository) GetStampCount(ctx context.Context, since time.Time, until time.Time) ([]StampCountResult, error) {
 	results := []StampCountResult{}
+	sinceDate := since.Truncate(24 * time.Hour)
+	untilDate := until.Truncate(24 * time.Hour)
 	query := `
 		SELECT
 			stamp_id,
-			SUM(reaction_count + message_count) AS total_count
+			reaction_count,
+			message_count
 		FROM
 			stamp_daily_usages
 		WHERE
 			date BETWEEN ? AND ?
 		GROUP BY
-			stamp_id
-		ORDER BY
-			total_count DESC;
+			stamp_id;
 	`
-	if err := r.db.SelectContext(ctx, &results, query, Since, Until); err != nil {
+	if err := r.db.SelectContext(ctx, &results, query, sinceDate, untilDate); err != nil {
 		return nil, fmt.Errorf("failed to get stamp count: %w", err)
 	}
 
