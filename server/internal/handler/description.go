10ce40c93d451025159354809b1102b720a14bcf
Ryuya HASEGAWA <ryhaxa@gmail.com>
2025-08-21 13:48:22 +0900

    getDescriptionsの実装

--- server/internal/handler/description.go
+++ server/internal/handler/description.go
@@ -3,18 +3,31 @@ package handler
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
 func (h *Handler) createDescriptions(c echo.Context) error {
 	return c.String(http.StatusOK, "pong")
 }
+
 func (h *Handler) getDescriptions(c echo.Context) error {
-	return c.String(http.StatusOK, "pong")
+	stampID, err := uuid.Parse(c.Param("stampId"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+	descriptions, err := h.repo.GetDescriptionsByStampID(c.Request().Context(), stampID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
+	}
+
+	return c.JSON(http.StatusOK, descriptions)
 }
+
 func (h *Handler) updateDescriptions(c echo.Context) error {
 	return c.String(http.StatusOK, "pong")
 }
+
 func (h *Handler) deleteDescriptions(c echo.Context) error {
 	return c.String(http.StatusOK, "pong")
 }
