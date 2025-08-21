2b7322a788a1028a7fa1ae6422873467b9cc1108
ikea-6330 <ide.k.624d@m.isct.ac.jp>
2025-08-22 01:36:09 +0900

    カレンダー部分の修正

--- client/app/components/SearchInputModal.vue
+++ client/app/components/SearchInputModal.vue
@@ -17,22 +17,61 @@
     <UFormField
       label="タグ名を指定"
       required
+      icon="i-lucide-search"
     >
       <UInputTags
-        :v-model="searchStampTagValue"
-        placeholder="タグ名を入力"
+        v-model="searchStampTagValue"
+        placeholder="タグ名を入力してください"
         class="mb-5 w-full"
       />
     </UFormField>
     <p>作成日を指定</p>
-    <CarendarModal />
-  </ucontainer>
+    <UPopover>
+      <UButton
+        color="neutral"
+        variant="subtle"
+        icon="i-lucide-calendar"
+      >
+        <template v-if="searchDateValue.start">
+          <template v-if="searchDateValue.end">
+            {{ df.format(searchDateValue.start.toDate(getLocalTimeZone())) }} - {{ df.format(searchDateValue.end.toDate(getLocalTimeZone())) }}
+          </template>
+
+          <template v-else>
+            {{ df.format(searchDateValue.start.toDate(getLocalTimeZone())) }}
+          </template>
+        </template>
+        <template v-else>
+          日付を選択
+        </template>
+      </UButton>
+
+      <template #content>
+        <UCalendar
+          v-model="searchDateValue"
+          class="p-2"
+          :number-of-months="1"
+          range
+        />
+      </template>
+    </UPopover>
+  </UContainer>
 </template>
 
 <script setup lang="ts">
-import CarendarModal from './CarendarModal.vue';
+import { DateFormatter, getLocalTimeZone, today } from '@internationalized/date';
 
 const searchDescriptionValue = ref('');
 const searchStampNameValue = ref('');
-const searchStampTagValue = ref(['Vue']);
+const searchStampTagValue = ref<string[]>([]);
+
+const df = new DateFormatter('jp-JP', {
+  dateStyle: 'medium',
+});
+
+const now = today('Asia/Tokyo');
+const searchDateValue = shallowRef({
+  start: now.add({ months: -1 }),
+  end: now.add({ days: -1 }),
+});
 </script>
