2c8956afd503f92d81d7e597d9f26299cd4bca82
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-08-21 13:50:46 +0900

    feat: grid レイアウトのCSSの調整

--- client/app/components/StampGrid.vue
+++ client/app/components/StampGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <UContainer>
-    <div class="grid grid-cols-3 gap-5">
+    <div class="grid gap-x-4 gap-y-6 grid-cols-[repeat(auto-fill,_96px)] justify-between content-start items-start">
       <StampItem
         v-for="stampItem in stampTestData"
         :key="stampItem.id"
@@ -21,6 +21,71 @@ const stampTestData: stampSummary[] = [
     id: '0197a63e-44f2-7779-b843-c805c52baacc',
     name: 'korosu-nya',
     file_id: '0197a63e-4471-7779-a0f8-8225104a20ce',
+  },
+  {
+    id: '0197a69d-c3ce-7822-9666-ace99bd35068',
+    name: '403_forbidden',
+    file_id: '0197a69d-c383-7822-84da-332d68451471',
+  },
+  {
+    id: 'e1e4a295-cf24-4de9-936c-e72469170d8f',
+    name: 'kyapi-nya',
+    file_id: 'e247c3ea-077c-4817-a7cd-8e0185d576d3',
+  },
+  {
+    id: '0197a63e-44f2-7779-b843-c805c52baacc',
+    name: 'korosu-nya',
+    file_id: '0197a63e-4471-7779-a0f8-8225104a20ce',
+  },
+  {
+    id: '0197a69d-c3ce-7822-9666-ace99bd35068',
+    name: '403_forbidden',
+    file_id: '0197a69d-c383-7822-84da-332d68451471',
+  },
+  {
+    id: 'e1e4a295-cf24-4de9-936c-e72469170d8f',
+    name: 'kyapi-nya',
+    file_id: 'e247c3ea-077c-4817-a7cd-8e0185d576d3',
+  },
+  {
+    id: '0197a63e-44f2-7779-b843-c805c52baacc',
+    name: 'korosu-nya',
+    file_id: '0197a63e-4471-7779-a0f8-8225104a20ce',
+  },
+  {
+    id: '0197a69d-c3ce-7822-9666-ace99bd35068',
+    name: '403_forbidden',
+    file_id: '0197a69d-c383-7822-84da-332d68451471',
+  },
+  {
+    id: 'e1e4a295-cf24-4de9-936c-e72469170d8f',
+    name: 'kyapi-nya',
+    file_id: 'e247c3ea-077c-4817-a7cd-8e0185d576d3',
+  },
+  {
+    id: '0197a63e-44f2-7779-b843-c805c52baacc',
+    name: 'korosu-nya',
+    file_id: '0197a63e-4471-7779-a0f8-8225104a20ce',
+  },
+  {
+    id: '0197a69d-c3ce-7822-9666-ace99bd35068',
+    name: '403_forbidden',
+    file_id: '0197a69d-c383-7822-84da-332d68451471',
+  },
+  {
+    id: 'e1e4a295-cf24-4de9-936c-e72469170d8f',
+    name: 'kyapi-nya',
+    file_id: 'e247c3ea-077c-4817-a7cd-8e0185d576d3',
+  },
+  {
+    id: '0197a63e-44f2-7779-b843-c805c52baacc',
+    name: 'korosu-nya',
+    file_id: '0197a63e-4471-7779-a0f8-8225104a20ce',
+  },
+  {
+    id: '0197a69d-c3ce-7822-9666-ace99bd35068',
+    name: '403_forbidden',
+    file_id: '0197a69d-c383-7822-84da-332d68451471',
   },
 ];
 </script>
--- client/app/components/StampItem.vue
+++ client/app/components/StampItem.vue
@@ -3,8 +3,8 @@
     variant="soft"
     class="hover:bg-primary-100"
     :ui="{
-      root: 'p-2',
-      footer: 'p-1',
+      root: 'p-2 w-1/7 min-w-24',
+      footer: 'px-1 sm:px-1 py-1 mx-0',
     }"
   >
     <NuxtImg :src="`https://q.trap.jp/api/1.0/public/emoji/${stampItem.id}`" />
