31144e457b3940ff39d7e28ac2c2c0da41231327
Ryuya HASEGAWA <ryhaxa@gmail.com>
2025-08-21 14:47:47 +0900

    createDescriptionsの実装

--- server/internal/handler/description.go
+++ server/internal/handler/description.go
@@ -5,10 +5,32 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/traP-jp/1m25_11/server/internal/repository"
 )
 
 func (h *Handler) createDescriptions(c echo.Context) error {
-	return c.String(http.StatusOK, "pong")
+	stampID, err := uuid.Parse(c.Param("stamp_id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+	creatorID, err := uuid.Parse(c.Param("creator_id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+	description := c.Param("description")
+	if description == "" {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+	err = h.repo.CreateDescriptions(c.Request().Context(), repository.CreateDescriptionParams{
+		StampID:     stampID,
+		Description: description,
+		CreatorID:   creatorID,
+	})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
+	}
+
+	return c.NoContent(http.StatusCreated)
 }
 
 func (h *Handler) getDescriptions(c echo.Context) error {
@@ -33,8 +55,11 @@ func (h *Handler) updateDescriptions(c echo.Context) error {
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
 	}
-	err = h.repo.UpdateDescriptions(c.Request().Context(), stampID, creatorID, c.Param("description"))
-	if err != nil {
+	description := c.Param("description")
+	if description == "" {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+	if err = h.repo.UpdateDescriptions(c.Request().Context(), stampID, creatorID, description); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
 	}
 
