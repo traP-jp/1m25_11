c5bc6f9a3a6d5d5fd00239fccf30642154a742d3
Ryuya HASEGAWA <ryhaxa@gmail.com>
2025-08-23 16:10:29 +0900

    createStampTags, deleteStampTagsの実装

--- server/internal/handler/stampTag.go
+++ server/internal/handler/stampTag.go
@@ -3,12 +3,46 @@ package handler
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/traP-jp/1m25_11/server/internal/repository"
 )
 
 func (h *Handler) createStampTags(c echo.Context) error {
-	return c.String(http.StatusOK, "pong")
+	stampID, err := uuid.Parse(c.Param("stampId"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+	tagID, err := uuid.Parse(c.Param("tagId"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+	creatorID := uuid.Nil // 仮でNil UUIDを用いている
+	err = h.repo.CreateStampTags(c.Request().Context(), repository.CreateStampTagParams{
+		StampID:   stampID,
+		TagID:     tagID,
+		CreatorID: creatorID,
+	})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
+	}
+
+	return c.NoContent(http.StatusCreated)
 }
 func (h *Handler) deleteStampTags(c echo.Context) error {
-	return c.String(http.StatusOK, "pong")
+	stampID, err := uuid.Parse(c.Param("stampId"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+	tagID, err := uuid.Parse(c.Param("tagId"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+
+	err = h.repo.DeleteStampTags(c.Request().Context(), stampID, tagID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
