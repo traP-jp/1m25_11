e93dfa4b0627f5dce22fea4a49478d886e4a5fdf
ikea-6330 <ide.k.624d@m.isct.ac.jp>
2025-08-27 18:55:09 +0900

    feat: タグ詳細ページの作成

---
+++ client/app/pages/tag/[tagName].vue
@@ -0,0 +1,35 @@
+<template>
+  <UContainer>
+    <h1 class="text-3xl font-bold mb-4">
+      タグ詳細
+    </h1>
+
+    <h2 class="text-xl font-semibold mb-2">
+      #{{ route.params.tagName }}
+    </h2>
+    <dl class="space-y-2 mb-6">
+      <div class="flex justify-between">
+        <dt class="font-medium">
+          作られた日
+        </dt>
+        <dd />
+      </div>
+      <div class="flex justify-between">
+        <dt class="font-medium">
+          作った人
+        </dt>
+        <dd />
+      </div>
+      <div class="flex justify-between">
+        <dt class="font-medium">
+          スタンプの数
+        </dt>
+      </div>
+      <StampGrid />
+    </dl>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+</script>
