2bd60b561ca16706b356d6234e1e9b7054e780ce
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-08-30 08:38:38 +0900

    openapi-fetchを、ES Module としてインポートするように修正

--- client/app/composables/ApiClient.ts
+++ client/app/composables/ApiClient.ts
@@ -1,4 +1,4 @@
-import createClient from 'openapi-fetch/dist/index.cjs';
+import createClient from 'openapi-fetch';
 import type { paths } from '~~/shared/types/generated';
 
 export const apiClient = createClient<paths>({
