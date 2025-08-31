bb06c435e781e7a9628a5627df72e539e4f0a8ee
hachimitsu-pichi <hachimitsu.38@icloud.com>
2025-08-31 16:09:38 +0900

    途中

--- server/internal/handler/ranking.go
+++ server/internal/handler/ranking.go
@@ -16,6 +16,11 @@ type errorResponse struct {
 	Message string `json:"message"`
 }
 
+type countResponse struct {
+	Reaction int `json:"reaction"`
+	Message  int `json:"message"`
+}
+
 type stampResponse struct {
 	ID     uuid.UUID `json:"id"`
 	Name   string    `json:"name"`
@@ -24,7 +29,7 @@ type stampResponse struct {
 
 type rankingResultResponse struct {
 	Stamp stampResponse `json:"stamp"`
-	Count int           `json:"count"`
+	Count countResponse `json:"count"`
 }
 
 func (h *Handler) getRanking(c echo.Context) error {
@@ -54,7 +59,7 @@ func (h *Handler) getRanking(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
 	}
 
-	var response []rankingResultResponse
+	response := make([]rankingResultResponse, 0, len(rankingResults))
 	for _, result := range rankingResults {
 		response = append(response, rankingResultResponse{
 			Stamp: stampResponse{
@@ -62,9 +67,17 @@ func (h *Handler) getRanking(c echo.Context) error {
 				Name:   result.Name,
 				FileID: result.FileID,
 			},
-			Count: result.ReactionCount,
+			Count: countResponse{
+				Reaction: result.ReactionCount,
+				Message:  result.MessageCount,
+			},
 		})
 	}
 
 	return c.JSON(http.StatusOK, response)
+<<<<<<< HEAD
 }
+=======
+}
+
+>>>>>>> 4a885af (途中)
