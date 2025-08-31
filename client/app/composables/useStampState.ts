f2f747e6874fd2fa9d0af9d341d97315cbf37784
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-08-31 12:25:52 +0900

    useIsStampDrawerOpen は要らないので削除

--- client/app/composables/useStampState.ts
+++ client/app/composables/useStampState.ts
@@ -1,8 +1,2 @@
 // drawerで閲覧しているスタンプのid
 export const useSelectedStampId = () => useState<string | null>('selected-stamp-id', () => null);
-
-// drawerが開いているかどうか
-export const useIsStampDrawerOpen = () => {
-  const selectedStampId = useSelectedStampId();
-  return computed(() => selectedStampId.value !== null);
-};
