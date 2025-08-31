9ffd1ae5650426ee519a8bfc07f5b5e181ed1897
hachimitsu-pichi <hachimitsu.38@icloud.com>
2025-08-31 15:03:50 +0900

    ranking 変更途中

--- server/internal/handler/ranking.go
+++ server/internal/handler/ranking.go
@@ -1,52 +1,77 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
-	"github.com/oapi-codegen/runtime/types"
-	"github.com/traP-jp/1m25_11/server/api"
 )
 
+type getRankingParams struct {
+	Since *time.Time `query:"since"`
+	Until *time.Time `query:"until"`
+}
+
+type errorResponse struct {
+	Message string `json:"message"`
+}
+
+type countResponse struct {
+	Reaction int `json:"reaction"`
+	Message  int `json:"message"`
+}
+
+type stampResponse struct {
+	ID     uuid.UUID `json:"id"`
+	Name   string    `json:"name"`
+	FileID uuid.UUID `json:"fileId"`
+}
+
+type rankingResultResponse struct {
+	Stamp stampResponse `json:"stamp"`
+	Count countResponse `json:"count"`
+}
+
 func (h *Handler) getRanking(c echo.Context) error {
-	var params api.GetStampsRankingParams
+	var params getRankingParams
 	if err := c.Bind(&params); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, fmt.Errorf("invalid parameters: %w", err))
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
 	}
+
 	since := time.Now().AddDate(0, 0, -30)
 	if params.Since != nil {
-		since = params.Since.Time
+		since = *params.Since
 	}
 
 	until := time.Now().AddDate(0, 0, -1)
 	if params.Until != nil {
-		until = params.Until.Time
+		until = *params.Until
 	}
 
 	if since.After(until) {
-		return echo.NewHTTPError(http.StatusBadRequest, api.Error{
+		return echo.NewHTTPError(http.StatusBadRequest, errorResponse{
 			Message: "Invalid date range: `since` must be before or on `until`.",
 		})
 	}
 
 	rankingResults, err := h.repo.GetStampCount(c.Request().Context(), since, until)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, api.Error{
-			Message: fmt.Sprintf("Failed to get stamp ranking: %s", err.Error()),
-		})
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
 	}
 
-	var response []api.RankingResult
+	var response []rankingResultResponse
 	for _, result := range rankingResults {
-		response = append(response, api.RankingResult{
-			Stamp: api.Stamp{
-				Id:     result.StampID,
-				Name:   "",
-				FileId: types.UUID{},
+		response = append(response, rankingResultResponse{
+			Stamp: stampResponse{
+				ID:     result.StampID,
+				Name:   result.Name,
+				FileID: result.FileID,
 			},
-			Count: result.ReactionCount,
+			Count: countResponse{
+				Reaction: result.ReactionCount,
+				Message:  result.MessageCount,
+			},
 		})
 	}
 
