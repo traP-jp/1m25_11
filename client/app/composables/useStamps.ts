4729d2c36bf6ce9ec67f190c542f12c061b01289
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-09-02 20:34:09 +0900

    スタンプ名を用いたStampSummary取得機能, スタンプ詳細情報の取得機能を実装

--- client/app/composables/useStamps.ts
+++ client/app/composables/useStamps.ts
@@ -1,15 +1,20 @@
-const stampsList = () => useState<StampSummary[]>('stamps-list', () => []);
-const stampsMap = () => useState<Map<string, StampSummary>>('stamps-map', () => new Map());
+const stampsList = () => useState<Schemas['StampSummary'][]>('stamps-list', () => []);
+const stampsMap = () => useState<Map<string, Schemas['StampSummary']>>('stamps-map', () => new Map());
 
 export const useStamps = () => {
   const list = readonly(stampsList());
   const map = readonly(stampsMap());
 
-  const getStampById = (stampId: string): StampSummary | undefined => map.value.get(stampId);
+  const getStampById = (stampId: string | undefined): Schemas['StampSummary'] | undefined => stampId ? map.value.get(stampId) : map.value.get('bc9a3814-f185-4b3d-ac1f-3c8f12ad7b52');
+
+  const getStampByName = (stampName: string): Schemas['StampSummary'] | undefined => {
+    return list.value.find(stamp => stamp.stamp_name === stampName);
+  };
 
   return {
     stampsList: list,
     stampsMap: map,
     getStampById,
+    getStampByName,
   };
 };
