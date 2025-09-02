fba15fa5cd28028f2113f5f712f2053ffcbde8ac
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-09-02 20:37:24 +0900

    stamps/{stampId} ではなく stamps/{stampName} になるように修正

--- client/app/pages/stamp/[stampName].vue
+++ client/app/pages/stamp/[stampName].vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 const stamps = useStamps();
 const route = useRoute();
-const stampId = stamps.getStampById(route.params.id as string)?.file_id;
+const stampName = stamps.getStampByName(route.params.stampName as string)?.file_id;
 </script>
 
 <template>
-  <div v-if="stampId">
-    {{ stampId }}
-    <StampDetailDrawer />
+  <div v-if="stampName">
+    {{ stampName }}
+    <!-- <StampDetail /> -->
   </div>
   <div v-else>
     スタンプが見つかりません
