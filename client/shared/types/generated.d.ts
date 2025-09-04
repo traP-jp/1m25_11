2043c077c89c01adfcd7440cbce9ed1f908cc7c3
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-09-04 18:02:46 +0900

    user-list エンドポイントを追加

--- client/shared/types/generated.d.ts
+++ client/shared/types/generated.d.ts
@@ -1014,6 +1014,48 @@ export interface paths {
     patch?: never;
     trace?: never;
   };
+  '/user-list': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    get: {
+      parameters: {
+        query?: never;
+        header?: never;
+        path?: never;
+        cookie?: never;
+      };
+      requestBody?: never;
+      responses: {
+        /** @description 成功 */
+        200: {
+          headers: {
+            [name: string]: unknown;
+          };
+          content: {
+            'application/json': components['schemas']['UserProfile'][];
+          };
+        };
+        /** @description 認証エラー */
+        401: {
+          headers: {
+            [name: string]: unknown;
+          };
+          content?: never;
+        };
+      };
+    };
+    put?: never;
+    post?: never;
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
 }
 export type webhooks = Record<string, never>;
 export interface components {
