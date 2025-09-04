fb61fe60eff15d03ef32b073098719d9ebb95462
Ryuya HASEGAWA <ryhaxa@gmail.com>
2025-09-04 11:50:32 +0900

    途中

--- server/internal/repository/updateUsage.go
+++ server/internal/repository/updateUsage.go
@@ -1,23 +1,6 @@
 package repository
 
-import (
-	"errors"
-	"net/http"
-	"os"
-)
-
 func (r *Repository) updateUsage() error {
-	botKey, ok := os.LookupEnv("BOT_TOKEN_KEY")
-	if !ok {
-		return errors.New("BOT_TOKEN_KEY not found in environment variables")
-	}
-
-	const url = "https://q.trap.jp/api/v3/stamps"
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return err
-	}
 
 	return nil
 }
