3ad5a5f92d14cf81686ffb55590f947d01064d02
mamo <p04snao11@gmail.com>
2025-09-05 13:37:23 +0900

    creatorIDの設定

--- server/internal/handler/creator.go
+++ server/internal/handler/creator.go
@@ -7,34 +7,9 @@ import (
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"strings"
-	"time"
 )
 
-type IDToken struct {
-	Audience          string `json:"aud"`
-	ExpiresAt         int64  `json:"exp"`
-	IssuedAt          int64  `json:"iat"`
-	Issuer            string `json:"iss"`
-	Name              string `json:"name"`
-	Picture           string `json:"picture"`
-	PreferredUsername string `json:"preferred_username"`
-	Subject           string `json:"sub"`
-	Traq              Traq   `json:"traq"`
-	UpdatedAt         int64  `json:"updated_at"`
-}
-type Traq struct {
-	Bio         string    `json:"bio"`
-	Bot         bool      `json:"bot"`
-	DisplayName string    `json:"display_name"`
-	Groups      []string  `json:"groups"`
-	HomeChannel string    `json:"home_channel"`
-	IconFileID  string    `json:"icon_file_id"`
-	LastOnline  time.Time `json:"last_online"`
-	Permissions []string  `json:"permissions"`
-	State       int       `json:"state"`
-	Tags        []string  `json:"tags"`
-	TwitterID   string    `json:"twitter_id"`
-}
+
 
 func (h *Handler) getUserID(c echo.Context) (uuid.UUID, error) {
 	cookie, err := c.Cookie(tokenKey)
@@ -48,16 +23,22 @@ func (h *Handler) getUserID(c echo.Context) (uuid.UUID, error) {
 	if err != nil {
 		return uuid.Nil, echo.NewHTTPError(http.StatusBadRequest, "failed to decode token payload")
 	}
-	var idToken IDToken
+	var payloadMap map[string]interface{}
 
-	if err := json.Unmarshal(payloadBytes, &idToken); err != nil {
-		return uuid.Nil, echo.NewHTTPError(http.StatusInternalServerError, "failed to parse token payload")
+	if err := json.Unmarshal(payloadBytes, &payloadMap); err != nil {
+        return uuid.Nil, echo.NewHTTPError(http.StatusInternalServerError, "failed to parse token payload")
+    }
+
+	creatorIDStr, ok := payloadMap["sub"].(string)
+	if !ok {
+		return uuid.Nil, echo.NewHTTPError(http.StatusBadRequest, "invalid token payload")
 	}
-
-	creatorID, err := uuid.Parse(idToken.Name)
+	creatorID, err := uuid.Parse(creatorIDStr)
 	if err != nil {
 		return uuid.Nil, echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
 	}
+
+
 
 	return creatorID, nil
 }
--- server/internal/handler/user.go
+++ server/internal/handler/user.go
@@ -3,106 +3,20 @@ package handler
 import (
 	"net/http"
 
-
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
-type (
-	GetUsersResponse []GetUserResponse
-
-	GetUserResponse struct {
-		ID               uuid.UUID      `json:"user_id"`
-		IsAdmin          bool           `json:"is_admin"`
-		StampsUserOwned  []StampSummary `json:"stamps_user_owned"`
-		TagsUserCreated  []TagSummary   `json:"tags_user_created"`
-		StampsUserTagged []struct {
-			Stamp StampSummary `json:"stamp"`
-			Tag   TagSummary   `json:"tag"`
-		} `json:"stamps_user_tagged"`
-		DescriptionsUserCreated []struct {
-			Stamp         StampSummary `json:"stamp"`
-			DescriptionID uuid.UUID    `json:"description_id"`
-		} `json:"descriptions_user_created"`
-	}
-
-	
-)
-
-
 
 func (h *Handler) GetUser(c echo.Context) error {
-	userID:= uuid.Nil
-
-	user, err := h.repo.GetUser(c.Request().Context(), userID)
+	creatorID, err := h.getUserID(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized).SetInternal(err)
+	}
+	user, err := h.repo.GetUser(c.Request().Context(), creatorID)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
 	}
 
-	// stampsUserOwned := make([]StampSummary, len(user.StampsUserOwned))
-	// for i, s := range user.StampsUserOwned {
-	// 	stampsUserOwned[i] = StampSummary{
-	// 		Id:     s.ID,
-	// 		Name:   s.Name,
-	// 		FileId: s.FileID,
-	// 	}
-	// }
-
-	// tagsUserCreated := make([]TagSummary, len(user.TagsUserCreated))
-	// for i, t := range user.TagsUserCreated {
-	// 	tagsUserCreated[i] = TagSummary{
-	// 		Id:   t.ID,
-	// 		Name: t.Name,
-	// 	}
-	// }
-
-	// stampsUserTagged := make([]struct {
-	// 	Stamp StampSummary `json:"stamp"`
-	// 	Tag   TagSummary   `json:"tag"`
-	// }, len(user.StampsUserTagged))
-	// for i, st := range user.StampsUserTagged {
-	// 	stampsUserTagged[i] = struct {
-	// 		Stamp StampSummary `json:"stamp"`
-	// 		Tag   TagSummary   `json:"tag"`
-	// 	}{
-	// 		Stamp: StampSummary{
-	// 			Id:     st.Stamp.ID,
-	// 			Name:   st.Stamp.Name,
-	// 			FileId: st.Stamp.FileID,
-	// 		},
-	// 		Tag: TagSummary{
-	// 			Id:   st.Tag.ID,
-	// 			Name: st.Tag.Name,
-	// 		},
-	// 	}
-	// }
-
-	// descriptionsUserCreated := make([]struct {
-	// 	Stamp         StampSummary `json:"stamp"`
-	// 	DescriptionID uuid.UUID    `json:"description_id"`
-	// }, len(user.DescriptionsUserCreated))
-	// for i, d := range user.DescriptionsUserCreated {
-	// 	descriptionsUserCreated[i] = struct {
-	// 		Stamp         StampSummary `json:"stamp"`
-	// 		DescriptionID uuid.UUID    `json:"description_id"`
-	// 	}{
-	// 		Stamp: StampSummary{
-	// 			Id:     d.Stamp.ID,
-	// 			Name:   d.Stamp.Name,
-	// 			FileId: d.Stamp.FileID,
-	// 		},
-	// 		DescriptionID: d.DescriptionID,
-	// 	}
-	// }
-
-	// res := GetUserResponse{
-	// 	ID:                      user.ID,
-	// 	IsAdmin:                 user.IsAdmin,
-	// 	StampsUserOwned:         stampsUserOwned,
-	// 	TagsUserCreated:         tagsUserCreated,
-	// 	StampsUserTagged:        stampsUserTagged,
-	// 	DescriptionsUserCreated: descriptionsUserCreated,
-	// }
 
 	return c.JSON(http.StatusOK, user)
 }
