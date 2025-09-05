2ccf9282100ab9338d1e6a3b9ebe464b85f6301c
Ryuya HASEGAWA <ryhaxa@gmail.com>
2025-09-05 14:17:54 +0900

    step2は多分完成した

--- analytics/production/step2_fetch.py
+++ analytics/production/step2_fetch.py
@@ -8,15 +8,19 @@ import requests
 bearer_token = os.environ.get("BEARER_TOKEN")
 auth_token = os.getenv('TRAQ_AUTH_TOKEN')
 
+max_messages = 10 # スタンプごとに取得する最大メッセージ数。最終的にはwhileの条件式が常にTrueになる程度に大きくする
+messages_per_request = 5 # 1回のリクエストで取得するメッセージ数。テスト用に小さくしているが、本番では100にする
+
+since = "2006-01-02T15:04:05Z"
+until = "2026-01-02T15:04:05Z"
+
 try:
     # with文でファイルを開く
     with open('targeted_stamps.json', 'r', encoding='utf-8') as f_stamps:
         stamps = json.load(f_stamps)
 
-    all_requests = []
-
-    all_traQ_data = []
-    all_traQing_data = []
+    all_traQ_messages = []
+    all_traQing_messages = []
 
     for stamp in stamps:
         # traQからデータ取得
@@ -25,49 +29,62 @@ try:
             "accept": "application/json",
             "Authorization": f"Bearer {bearer_token}"
         }
-        traQ_params = {
-            "word": f"\":{stamp['name']}:\"",
-            "after": "2006-01-02T15:04:05Z",
-            "before": "2026-01-02T15:04:05Z",
-            "bot": "false",
-            "limit": "10",
-            "offset": "0",
-            "sort": "createdAt",
-        }
+        traQ_messages_count = 0
+        traQ_messages = []
+        while traQ_messages_count < max_messages:
+            traQ_params = {
+                "word": f"\":{stamp['name']}:\"",
+                "after": "2006-01-02T15:04:05Z",
+                "before": "2026-01-02T15:04:05Z",
+                "bot": "false",
+                "limit": str(messages_per_request),
+                "offset": str(traQ_messages_count),
+                "sort": "createdAt"
+            }
 
-        traQ_response = requests.get(traQ_url, params=traQ_params, headers=traQ_headers)
-        traQ_response.raise_for_status()
-        traQ_data = traQ_response.json()['hits']
-        all_traQ_data.append({
+            traQ_response = requests.get(traQ_url, params=traQ_params, headers=traQ_headers)
+            traQ_response.raise_for_status()
+            traQ_messages.extend(traQ_response.json()['hits'])
+            traQ_messages_count += messages_per_request # 404の場合もカウントするのでlen(traQ_response.json()['hits'])ではない
+            if len(traQ_response.json()['hits']) < messages_per_request:
+                break
+        all_traQ_messages.append({
             "stamp_name": stamp['name'],
             "stamp_id": stamp['id'],
-            "messages": traQ_data
+            "messages": traQ_messages
         })
 
         # traQingからデータ取得
         traQing_url = "https://traqing.cp20.dev/api/stamps"
-        traQing_params = {
-            "stampId": stamp['id'],
-            "isBot": "false",
-            "groupBy": "message",
-            "orderBy": "date",
-            "order": "asc",
-            "limit": "10",
-            "offset": "0",
-            "after": "2006-01-02T15:00:00.000Z",
-            "before": "2026-01-02T14:59:59.999Z"
-        }
         traQing_cookies = {
             'traq-auth-token': auth_token
         }
 
-        traQing_response = requests.get(traQing_url, params=traQing_params, cookies=traQing_cookies)
-        traQing_response.raise_for_status()
-        traQing_data = traQing_response.json()
+        traQing_messageIds_count = 0
+        traQing_messageIds = []
+        while traQing_messageIds_count < max_messages:
+            traQing_params = {
+                "stampId": stamp['id'],
+                "isBot": "false",
+                "groupBy": "message",
+                "orderBy": "date",
+                "order": "asc",
+                "limit": str(messages_per_request),
+                "offset": str(traQing_messageIds_count),
+                "after": "2006-01-02T15:00:00.000Z",
+                "before": "2026-01-02T14:59:59.999Z"
+            }
 
-        # traQing_dataからmessageIdを取得してtraQ APIで詳細情報を取得
+            traQing_response = requests.get(traQing_url, params=traQing_params, cookies=traQing_cookies)
+            traQing_response.raise_for_status()
+            traQing_messageIds.extend(traQing_response.json())
+            traQing_messageIds_count += messages_per_request
+            if len(traQing_response.json()) < messages_per_request:
+                break
+
+        # traQing_messageIdsからmessageIdを取得してtraQ APIで詳細情報を取得
         traQing_messages = []
-        for item in traQing_data:
+        for item in traQing_messageIds:
             message_id = item['message']
 
             # traQ APIでメッセージの詳細を取得
@@ -78,16 +95,16 @@ try:
                 traQing_messages.append(message_response.json())
             else:
                 print(f"メッセージ {message_id} の取得に失敗: {message_response.status_code}")
-        all_traQing_data.append({
+        all_traQing_messages.append({
             "stamp_name": stamp['name'],
             "stamp_id": stamp['id'],
             "messages": traQing_messages
         })
 
     with open('traQ_data.json', 'w', encoding='utf-8') as f:
-        json.dump(all_traQ_data, f, ensure_ascii=False, indent=2)
+        json.dump(all_traQ_messages, f, ensure_ascii=False, indent=2)
     with open('traQing_data.json', 'w', encoding='utf-8') as f:
-        json.dump(all_traQing_data, f, ensure_ascii=False, indent=2)
+        json.dump(all_traQing_messages, f, ensure_ascii=False, indent=2)
 
 #         # 整形
 
