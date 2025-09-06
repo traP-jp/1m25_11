3a179bb608e168698fb2118bdbf794d704c8bffb
mamo <p04snao11@gmail.com>
2025-09-07 04:57:26 +0900

    コメントアウト

--- server/internal/handler/middleware.go
+++ server/internal/handler/middleware.go
@@ -1,12 +1,12 @@
 package handler
 
 import (
-	"context"
+	// "context"
 	"net/http"
 	"strings"
 	"log"
 
-	"github.com/coreos/go-oidc/v3/oidc"
+	// "github.com/coreos/go-oidc/v3/oidc"
 	"github.com/labstack/echo/v4"
 )
 
@@ -45,23 +45,23 @@ func getToken(c echo.Context) string {
 	return ""
 }
 
-func getMe(c echo.Context, rawToken string) error {
-	config := &oidc.Config{
-		ClientID: clientID,
-	}
-	provider, err := oidc.NewProvider(c.Request().Context(), "https://q.trap.jp")
-	if err != nil {
-		return err
-	}
+// func getMe(c echo.Context, rawToken string) error {
+// 	config := &oidc.Config{
+// 		ClientID: clientID,
+// 	}
+// 	provider, err := oidc.NewProvider(c.Request().Context(), "https://q.trap.jp")
+// 	if err != nil {
+// 		return err
+// 	}
 
-	ctx := context.Background()
-	verifier := provider.Verifier(config)
+// 	ctx := context.Background()
+// 	verifier := provider.Verifier(config)
 
-	_, err = verifier.Verify(ctx, rawToken)
-	if err != nil {
+// 	_, err = verifier.Verify(ctx, rawToken)
+// 	if err != nil {
 
-		return err
-	}
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
